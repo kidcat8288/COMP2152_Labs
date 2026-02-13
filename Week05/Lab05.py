5e6449cbb753b8ab7e0044332d8962dac9ba573a
kidcat8288 <kamea8288@icloud.com>
2026-02-13 15:06:06 -0500

    completed lab 5

--- Week05/Lab05.py
+++ Week05/Lab05.py
@@ -29,13 +29,14 @@ def fib(n):
         int: The nth Fibonacci number
     """
     # TODO: Base case 1 - If n equals 0, return 0
-
+    if n == 0:
+        return 0
 
     # TODO: Base case 2 - If n equals 1, return 1
-
-
+    if n == 1:
+        return 1
     # TODO: Recursive case - Return fib(n-1) + fib(n-2)
-    pass  # Remove this line when you add your code
+    return fib(n-1) + fib(n-2)
 
 
 # Test cases for Fibonacci
@@ -60,37 +61,26 @@ print("=" * 50)
 
 
 def fizz_buzz(n):
-    """
-    Generate FizzBuzz sequence from 1 to n.
-
-    Rules:
-    - Divisible by 3 AND 5: "FizzBuzz"
-    - Divisible by 3 only: "Fizz"
-    - Divisible by 5 only: "Buzz"
-    - Otherwise: the number as a string
-
-    Parameters:
-        n (int): The upper limit (inclusive)
-
-    Returns:
-        list: List of strings following FizzBuzz rules
-    """
-    result = []
+   result = []
 
     # TODO: Loop from 1 to n (inclusive)
     # Hint: Use range(1, n + 1)
-
+   for i in range(1, n+1):
         # TODO: Check if divisible by BOTH 3 and 5 FIRST
         # Hint: if i % 3 == 0 and i % 5 == 0
+        if i % 3 == 0 and i % 5 == 0:
+             result.append("FizzBuzz")
 
         # TODO: Then check if divisible by 3 only
-
+        elif i % 3 == 0:
+            result.append("Fizz")
         # TODO: Then check if divisible by 5 only
-
+        elif i % 5 == 0:
+            result.append("Buzz")
         # TODO: Otherwise, append the number as a string
-        # Hint: result.append(str(i))
-
-    return result
+        else:
+            result.append(str(i))
+   return result
 
 
 # Test cases for FizzBuzz
@@ -125,65 +115,48 @@ print("=" * 50)
 
 
 # Part A: Iterative Solution
+
+
 def binary_search_iterative(nums, target):
-    """
-    Search for target in sorted array using iteration.
-
-    Parameters:
-        nums (list): Sorted list of integers
-        target (int): Value to find
-
-    Returns:
-        int: Index of target, or -1 if not found
-    """
     # TODO: Initialize left pointer to 0
     left = 0
 
-    # TODO: Initialize right pointer to len(nums) - 1
     right = len(nums) - 1
 
-    # TODO: While left <= right:
-    #   - Calculate mid = (left + right) // 2
-    #   - If nums[mid] == target, return mid
-    #   - If target < nums[mid], search left half: right = mid - 1
-    #   - If target > nums[mid], search right half: left = mid + 1
-
-
-    # TODO: Return -1 if target not found
+    while left <= right:
+      mid = (left + right) // 2
+      if nums[mid] == target:
+          return mid
+      elif target < nums[mid]:
+          right = mid - 1
+      else:
+          left= mid + 1
+  # TODO: Return -1 if target not found
     return -1
 
 
 # Part B: Recursive Solution
 def binary_search_recursive(nums, target, left, right):
-    """
-    Search for target in sorted array using recursion.
-
-    Parameters:
-        nums (list): Sorted list of integers
-        target (int): Value to find
-        left (int): Left boundary index
-        right (int): Right boundary index
-
-    Returns:
-        int: Index of target, or -1 if not found
-    """
+ 
     # TODO: Base case - If left > right, return -1 (target not found)
-
+    if left > right:
+        return -1
 
     # TODO: Calculate mid = (left + right) // 2
-
+    mid = (left + right) // 2
+    
 
     # TODO: If nums[mid] == target, return mid
-
+    if nums[mid] == target:
+        return mid
 
     # TODO: If target < nums[mid], recurse on left half
-    # Hint: return binary_search_recursive(nums, target, left, mid - 1)
+    if target < nums[mid]:
+        return binary_search_recursive(nums, target, left, mid-1)
+    if target > nums[mid]:
+        return binary_search_recursive(nums, target, mid+1, right)
 
 
-    # TODO: If target > nums[mid], recurse on right half
-    # Hint: return binary_search_recursive(nums, target, mid + 1, right)
-
-    pass  # Remove this line when you add your code
 
 
 # Wrapper function for recursive solution
